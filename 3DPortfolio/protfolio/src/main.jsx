e64696b9449f2f4d3c3bbbe1ef96241f0900aa85
saquib <saquibshahid508@gmail.com>
2024-03-26 17:31:24 +0530

    half done

---
+++ 3DPortfolio/protfolio/src/main.jsx
@@ -0,0 +1,5 @@
+import ReatDom from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReatDom.createRoot(document.getElementById('root')).render(<App/>);
---
+++ 3DPortfolio/protfolio/src/pages/index.js
@@ -0,0 +1,6 @@
+import Projects from './Projects.jsx';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
+import Home from './Home.jsx';
+
+export  {Home , Contact , Projects , About};
